7af02eaf349d46323f0483a192f07981dd0b1884
Rita Moraes <ritac.moraes@gmail.com>
2018-10-22 16:56:24 -0300

    Third Commit

---
+++ src/reducers/__tests__/comments.test.js
@@ -0,0 +1,21 @@
+import commentsReducer from 'reducers/comments';
+import { SAVE_COMMENT } from 'actions/types';
+
+it('handles actions of type SAVE_COMMENT', () => {
+    const action = {
+      type: SAVE_COMMENT,
+      payload: 'New Comment'
+    };
+
+    const newState = commentsReducer([], action);
+
+    expect(newState).toEqual(['New Comment'])
+});
+
+it('handle action with unknown  type', () => {
+    const newState = commentsReducer([], {type:'ASDHFLAKSHLK'});
+    expect(newState).toEqual([]);
+})
+
+
+
